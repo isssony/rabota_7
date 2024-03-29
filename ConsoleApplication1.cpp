1484caa34b60c4cded90b619c3d6481d8a10a298
Bodveahxx <165203128+Bodveahxx@users.noreply.github.com>
2024-03-29 10:06:30 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 10;
+
+	if (x < 11)
+	{
+		cout << "x < 11 is true!\n";  // executed
+	}
+	else
+	{
+		cout << "x < 11 is false!\n"; // not executed
+	}
+
+	// no else statement
+	bool flag = false;
+	if (flag == true)
+	{
+		x = 100; // not executed
+	}
+
+	int *p = new int(25);
+	if (p)
+	{
+		cout << *p << "\n"; // outputs 25
+	}
+	else
+	{
+		cout << "p is null!\n"; // executed if memory allocation fails
+	}
+}
